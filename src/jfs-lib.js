a5307d43525c6783fa143cd2801f67fb1278551f
Pål Andreassen <pal.andreassen@bouvet.no>
2017-02-06 22:27:36 +0100

    Fixed upload

--- src/jfs-lib.js
+++ src/jfs-lib.js
@@ -10,6 +10,110 @@ var querystring = require('querystring');
 
 var config = {};
 
+module.exports = {
+    setConfig : function (settings) {
+        config = settings;
+    },
+    list: list,
+    listDevices: listDevices,
+    getFile: getFile,
+    putFile: putFile,
+    putFolder: putFolder
+}
+
+function putFolder(remotePath, localFolder) {
+    uploader.uploadFolder(config, remotePath, localFolder);
+}
+
+function putFile (remotePath, localFile) {
+    uploader.uploadFile(config, remotePath, localFile);
+}
+
+function getFile(path) {
+    var url = 'https://down.jottacloud.com/jfs/' + config.username + '/' + querystring.escape(path) + '?mode=bin';
+    var target = path.substring(path.lastIndexOf('/')+1).replace(/((\?|#).*)?$/,'');
+    
+    console.log('Downloading: ' + path);
+    console.log('Saving to: ' + target);
+
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'node-jfs https://github.com/paaland/node-jfs'
+        }
+    };
+
+    request
+        .get(options)
+        .auth(config.username, config.password, true)
+        .on('error', function(err) {
+            console.error(err)
+        })
+        .on('response', function(response) {
+            console.log(response.statusCode) // 200
+        })
+        .pipe(fs.createWriteStream(target));
+}
+
+function listDevices () {
+    var url = 'https://www.jottacloud.com/jfs/' + config.username;
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'node-jfs https://github.com/paaland/node-jfs'
+        }
+    };
+
+    request.get(options, 
+        function (error, response, body) {
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            var user = et.parse(body);
+            var devices = user.findall('devices/device');
+
+            console.log('\n' + minLength('Type', 12) + '\t' + minLength('Size', 10) + '\tName');
+            devices.forEach(function(device) {
+                console.log(minLength(device.findtext('type'),12) + '\t' + minLength(prettyBytes(parseInt(device.findtext('size'), 10)), 10) + '\t' + device.findtext('name'));
+            });
+        }).auth(config.username, config.password, true);
+}
+
+function list(path) {
+    var url = 'https://www.jottacloud.com/jfs/' + config.username + '/' + path;
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'node-jfs https://github.com/paaland/node-jfs'
+        }
+    };
+
+    request.get(options, 
+        function (error, response, body) {
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            var xml = et.parse(body);
+            var root = xml.getroot().tag;
+
+            if (root == 'device')
+                device.list(xml);
+            else if (root == 'mountPoint')
+                mountpoint.list(xml);
+            else if (root == 'folder')
+                folder.list(xml);
+            else
+                console.error('Element type ' + root + ' not implemented');
+
+            //console.log(xml);
+
+        }).auth(config.username, config.password, true);
+}
+
 function minLength(text, minLen) {
     while (text.length < minLen)
     {
@@ -17,99 +121,4 @@ function minLength(text, minLen) {
     }
 
     return text;
-}
-
-module.exports = {
-    setConfig : function (settings) {
-        config = settings;
-    },
-    list: function (path) {
-        var url = 'https://www.jottacloud.com/jfs/' + config.username + '/' + path;
-        var options = {
-            url: url,
-            headers: {
-                'User-Agent': 'node-jfs https://github.com/paaland/node-jfs'
-            }
-        };
-
-        request.get(options, 
-            function (error, response, body) {
-                if (error) {
-                    console.error(error);
-                    return;
-                }
-
-                var xml = et.parse(body);
-                var root = xml.getroot().tag;
-
-                if (root == 'device')
-                    device.list(xml);
-                else if (root == 'mountPoint')
-                    mountpoint.list(xml);
-                else if (root == 'folder')
-                    folder.list(xml);
-                else
-                    console.error('Element type ' + root + ' not implemented');
-
-                //console.log(xml);
-
-            }).auth(config.username, config.password, true);
-    },
-    listDevices : function ()
-    {
-        var url = 'https://www.jottacloud.com/jfs/' + config.username;
-        var options = {
-            url: url,
-            headers: {
-                'User-Agent': 'node-jfs https://github.com/paaland/node-jfs'
-            }
-        };
-
-        request.get(options, 
-            function (error, response, body) {
-                if (error) {
-                    console.error(error);
-                    return;
-                }
-
-                var user = et.parse(body);
-                var devices = user.findall('devices/device');
-
-                console.log(minLength('Type', 12) + '\t' + minLength('Size', 10) + '\tName');
-                devices.forEach(function(device) {
-                    console.log(minLength(device.findtext('type'),12) + '\t' + minLength(prettyBytes(parseInt(device.findtext('size'), 10)), 10) + '\t' + device.findtext('name'));
-                });
-            }).auth(config.username, config.password, true);
-    },
-    getFile : function (path) {
-        var url = 'https://down.jottacloud.com/jfs/' + config.username + '/' + querystring.escape(path) + '?mode=bin';
-        var target = path.substring(path.lastIndexOf('/')+1).replace(/((\?|#).*)?$/,'');
-        
-        console.log('Downloading: ' + path);
-        console.log('Saving to: ' + target);
-
-        var options = {
-            url: url,
-            headers: {
-                'User-Agent': 'node-jfs https://github.com/paaland/node-jfs'
-            }
-        };
-
-        request
-            .get(options)
-            .auth(config.username, config.password, true)
-            .on('error', function(err) {
-                console.error(err)
-            })
-            .on('response', function(response) {
-                console.log(response.statusCode) // 200
-            })
-            .pipe(fs.createWriteStream(target));
-    },
-    putFile: function (remotePath, localFile) {
-        uploader.uploadFile(config, remotePath, localFile);
-    },
-    putFolder: function (remotePath, localFolder) {
-        uploader.uploadFolder(config, remotePath, localFolder);
-    }
 }
--- src/jfsuploader.js
+++ src/jfsuploader.js
@@ -1,10 +1,12 @@
 var request = require('request');
 var fs = require('fs');
-var md5 = require('md5');
 var querystring = require('querystring');
 var dateformat = require('dateformat');
 var async = require('async');
-const path = require('path');
+var crypto = require('crypto');
+var path = require('path');
+var prettyBytes = require('pretty-bytes');
+var et = require('elementtree');
 
 //For debug, accept self signed ssl 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -80,18 +82,47 @@ function uploadFolder (config, remotePath, localFolder) {
     // }, this);
 }
     
-function uploadFile(config, remotePath, localFile)
+function uploadFile(config, remotePath, localFile, callback)
+{    
+    // Calculate MD5 hash of file
+    var fd = fs.createReadStream(localFile);
+    var hash = crypto.createHash('md5');
+    hash.setEncoding('hex');
+
+    fd.on('end', function() {
+        hash.end();
+        var md5hash = hash.read(); 
+        
+        checkIfFileExists(config, remotePath, localFile, md5hash, function (status) {
+            if (status === 200) {
+                console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') +': File already exists, skipping.');
+                if (callback)
+                    callback();
+            } else if (status === 404) {
+                //File not already on remote, upload file
+                uploadFileToRemote(config, remotePath, localFile, md5hash, function (status) {
+                    console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') + ': File "' + localFile + '" uploaded, response ' + status); 
+                    if (callback)
+                        callback();                       
+                });
+            } else {
+                console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') +': ERROR: Unknown status ' + status);
+                if (callback)
+                    callback("Error");
+            }
+        });
+    });
+
+    // read all file and pipe it (write it) to the hash object
+    fd.pipe(hash);
+}
+
+function uploadFileToRemote(config, remotePath, localFile, md5hash, callback)
 {
     var fileName = path.basename(localFile);
-    var url = '';
     var stats = fs.statSync(localFile);        
-    var md5hash = md5(fs.readFileSync(localFile));
-    
-    console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') +': MD5 hash ' + md5hash);        
-    url = 'https://jfs.jottacloud.com/jfs/' + config.username + '/' + remotePath + '/' + fileName + '?cphash=' + md5hash;
-
     var options = {
-        url: url,
+        url: 'https://up.jottacloud.com/jfs/' + config.username + '/' + remotePath + '/' + fileName + '?umode=nomultipart',
         headers: {
             'User-Agent': 'node-jfs https://github.com/paaland/node-jfs',
             'JMd5': md5hash,
@@ -100,33 +131,55 @@ function uploadFile(config, remotePath, localFile)
             'JSize': stats.size
         }
     };
+
+    console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') + ': Uploading "' +  localFile + '", ' + prettyBytes(stats.size));
         
-    console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') +': Checking if file exists');
-    //Check if file with same name, size, md5hash, modified date and created date exists
-    request.post(options)
+    //Upload file
+    fs.createReadStream(localFile)
+        .pipe(request.post(options))
         .auth(config.username, config.password, true)
         .on('error', function(error) {
-            //Error means file does not exists, upload file                
-            console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') + ': Uploading "' +  localFile + '"');
+            console.error(error)
+            callback(error.statusCode);                        
+        })
+        .on('response', function(response) {
+            callback(response.statusCode);
         });
-        
-            //     //Change to upload URL
-            //     options.url = 'https://up.jottacloud.com/jfs/' + config.username + '/' + remotePath + '/' + fileName + '?umode=nomultipart';
-                
-            //     //Upload file
-            //     fs.createReadStream(localFile)
-            //         .pipe(request.post(options))
-            //         .auth(config.username, config.password, true)
-            //         .on('error', function(error) {
-            //             console.error(error)                        
-            //         })
-            //         .on('response', function(response) {
-            //             console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') + ': File "' + localFile + '" uploaded, response ' + response.statusCode);                        
-            //         });
-            //     })
-            // .on('response', function(response) {
-            //     if (response.statusCode == '200') {
-            //         console.log(dateformat(new Date(), 'dd.mm.yyyy HH:MM:ss') + ': File "' +  localFile + '" already exists, skipping.');                                       
-            //     }
-            // });
 }
+
+function checkIfFileExists(config, remotePath, localFile, md5hash, callback)
+{
+    var fileName = path.basename(localFile);
+    var stats = fs.statSync(localFile);        
+    var options = {
+        url: 'https://jfs.jottacloud.com/jfs/' + config.username + '/' + remotePath + '/' + fileName + '?cphash=' + md5hash,
+        headers: {
+            'User-Agent': 'node-jfs https://github.com/paaland/node-jfs',
+            'JMd5': md5hash,
+            'JCreated': stats.ctime,
+            'JModified': stats.mtime,
+            'JSize': stats.size
+        }
+    };
+    
+    //Check if file with same name, size, md5hash, modified date and created date exists
+    request.post(options, function (error, response, body) {
+         if (!error && response.statusCode == 200) {
+             if (fileIsComplete(body))
+                callback(response.statusCode);
+            else
+                callback(404);                
+         } else
+            callback(404);
+        })
+        .auth(config.username, config.password, true);
+}
+
+function fileIsComplete(body) 
+{
+    var file = et.parse(body);
+    var state = file.findtext('latestRevision/state');
+    
+    return state === 'COMPLETED';
+}
+        
